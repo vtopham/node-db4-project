8148abf98d5d5daf703c43560f96bae10c232dc9
Victoria  Topham <topham.victoria@gmail.com>
2020-05-14 12:49:51 -0700

    spun up a server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Go get em tiger"})
+})
+server.listen(4000, _ => {
+    console.log("Listening on port 4000")
+})
+
