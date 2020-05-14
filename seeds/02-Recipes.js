7602824e4e3afab8ec396e94792e71c679fd6731
Victoria  Topham <topham.victoria@gmail.com>
2020-05-14 13:02:19 -0700

    seeded ingredientslist and recipes

--- seeds/02-Recipes.js
+++ seeds/02-Recipes.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Recipes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('Recipes').insert([
+        {Author: "Your husband who you thought was dead", RecipeName: "A dream you can't remember"},
+        {Author: "A shadow, too blurry to make out", RecipeName: "Something soothing"},
+        {Author: "To your best recollection, nobody", RecipeName: "Your worst fears"},
       ]);
     });
 };
